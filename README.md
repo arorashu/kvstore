f6c7ef740f970af7166ca2cd2ddc6919c9c10fb5
Shubham Arora <aroras@bu.edu>
2020-08-31 23:52:34 -0400

    Load from file, show data on HTTP server. Close server using HTTP command

--- README.md
+++ README.md
@@ -10,3 +10,16 @@
 ```
 go run main.go
 ```
+
+## Steps
+
+1. commit: 71a5e584d415663ea3f06b4f0c4da1e21db589e3
+
+    We have a basic data structure KV, that supports Put, Get and Delete.
+    The Put is an O(1) operation, and Get and Delete are O(n) operations.
+    This is not a persistent store. i.e. the store is only available while your program is running.
+
+    Next steps, let's make this a persistent store.
+
+
+
--- main.go
+++ main.go
@@ -4,6 +4,11 @@ package main
 
 import (
 	"fmt"
+    "net/http"
+    "log"
+    "io/ioutil"
+    "os"
+    "strings"
 )
 
 type KV struct {
@@ -53,31 +58,100 @@ func (store *KV) Print() {
 
 	fmt.Printf("All pairs: \n{ ")
 	for i, _ := range store.keys {
-		//fmt.Printf("[key: %s, value: %s], ", store.keys[i], store.values[i])
 		fmt.Printf("[%s: %s], ", store.keys[i], store.values[i])
 	}
 	fmt.Printf(" }\n")
 }
 
+
+func (store *KV) ToString() string{
+	if len(store.keys) == 0 {
+		return ""
+	}
+    res := ""
+	for i, _ := range store.keys {
+		res += fmt.Sprintf("%s,%s\n", store.keys[i], store.values[i])
+	}
+    return res
+}
+
+func (store *KV) LoadFromFile(db string) {
+    data, err := ioutil.ReadFile(db + ".kv")
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "load: %v\n", err)
+        return
+    }
+    // each line is a key-value pair, separated by \n
+    // each line is key,value
+
+    for _, line := range strings.Split(string(data), "\n") {
+       if line != "" {
+           if words := strings.Split(line, ","); len(words)==2 {
+            store.Put(words[0], words[1])
+           } else {
+                fmt.Fprintf(os.Stderr, "load, unreadable line: %v, length: %d\n", line, len(words))
+           }
+       } // ignores empty lines
+    }
+}
+
+
+func (store *KV) WriteToFile(db string) {
+    data := store.ToString()
+    err := ioutil.WriteFile(db + ".kv", []byte(data), 0644)
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "write to file: %v\n", err)
+    }
+}
+
+
+//func showAll(w http.ResponseWriter, r *http.Request) {
+//  fmt.Fprintf(w, store.Print())
+//}
+
+
 func main() {
 	fmt.Println("Hello, kvstore")
-	store := KV{}
+    store := KV{}
+    store.LoadFromFile("db")
 	store.Put("hello", "world")
 	store.Put("cake", "walk")
-	store.Print()
-	val, _ := store.Get("hello")
-	fmt.Printf("key: hello, val: %s\n", val)
-	val, err := store.Get("hi")
-	if err {
-		fmt.Printf("key: hi, val: %s\n", val)
-	} else {
-		fmt.Printf("key: hi not found\n")
-	}
-	ok := store.Delete("cake")
-	if ok {
-		fmt.Printf("Deleted key: cake\n")
-	} else {
-		fmt.Printf("Cannot delete key: cake\n")
-	}
-	store.Print()
+
+    http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, store.ToString())
+    })
+
+    http.HandleFunc("/close", func (w http.ResponseWriter, r *http.Request) {
+        store.WriteToFile("db")
+        fmt.Fprintf(w, "closing kv server. Goodbye!")
+        fmt.Fprintf(os.Stdout, "closing kv server. Goodbye!")
+        os.Exit(0)
+    })
+
+
+//    http.HandleFunc("/put", func (w http.ResponseWriter, r *http.Request) {
+//        store.Put()
+//        fmt.Fprintf(w, "closing kv server. Goodbye!")
+//        fmt.Fprintf(os.Stdout, "closing kv server. Goodbye!")
+//        os.Exit(0)
+//})
+
+    log.Fatal(http.ListenAndServe(":8080", nil))
+
+//	store.Print()
+//	val, _ := store.Get("hello")
+//	fmt.Printf("key: hello, val: %s\n", val)
+//	val, err := store.Get("hi")
+//	if err {
+//		fmt.Printf("key: hi, val: %s\n", val)
+//	} else {
+//		fmt.Printf("key: hi not found\n")
+//	}
+//	ok := store.Delete("cake")
+//	if ok {
+//		fmt.Printf("Deleted key: cake\n")
+//	} else {
+//		fmt.Printf("Cannot delete key: cake\n")
+//	}
+//	store.Print()
 }
